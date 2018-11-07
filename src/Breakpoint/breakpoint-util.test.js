fd1944c593efb0128f9eb344f6d1b15d89236541
Adarsh <adarshan_naiynar@icloud.com>
2018-11-07 18:55:29 +0530

    Separated jest config and fixed logic

--- src/Breakpoint/breakpoint-util.test.js
+++ src/Breakpoint/breakpoint-util.test.js
@@ -13,24 +13,25 @@ describe('Breakpoint Util', () => {
   });
 
   it('throw error while setting breakpoints - not an array', () => {
-    // this wont work
-    // expect(setDefaultBreakpoints(null)).toThrow();
-    try {
-      setDefaultBreakpoints(null);
-    } catch (e) {
-      expect(e.message).toEqual(
-        'setDefaultBreakpoints error: Breakpoints should be an array of objects'
-      );
-    }
+    expect(() => setDefaultBreakpoints(null)).toThrowError(/^setDefaultBreakpoints error: Breakpoints should be an array$/);
+    expect(() => setDefaultBreakpoints("hello")).toThrowError(/^setDefaultBreakpoints error: Breakpoints should be an array$/);
+    expect(() => setDefaultBreakpoints({})).toThrowError(/^setDefaultBreakpoints error: Breakpoints should be an array$/);
+    expect(() => setDefaultBreakpoints(undefined)).toThrowError(/^setDefaultBreakpoints error: Breakpoints should be an array$/);
+    expect(() => setDefaultBreakpoints(1.0)).toThrowError(/^setDefaultBreakpoints error: Breakpoints should be an array$/);
+    expect(() => setDefaultBreakpoints(true)).toThrowError(/^setDefaultBreakpoints error: Breakpoints should be an array$/);
+    expect(() => setDefaultBreakpoints(false)).toThrowError(/^setDefaultBreakpoints error: Breakpoints should be an array$/);
   });
 
+  it('throw error while setting breakpoints - not an array of objects', () => {
+    expect(() => setDefaultBreakpoints([null])).toThrowError(/^setDefaultBreakpoints error: Breakpoints should be an array of objects$/);
+    expect(() => setDefaultBreakpoints(["hello"])).toThrowError(/^setDefaultBreakpoints error: Breakpoints should be an array of objects$/);
+    expect(() => setDefaultBreakpoints([undefined])).toThrowError(/^setDefaultBreakpoints error: Breakpoints should be an array of objects$/);
+    expect(() => setDefaultBreakpoints([1.0])).toThrowError(/^setDefaultBreakpoints error: Breakpoints should be an array of objects$/);
+    expect(() => setDefaultBreakpoints([true, false])).toThrowError(/^setDefaultBreakpoints error: Breakpoints should be an array of objects$/);
+  });
+  
   it('throw error while setting breakpoints - wrong object format', () => {
-    try {
-      setDefaultBreakpoints([{ a: 1010, b: 1010 }]);
-    } catch (e) {
-      expect(e.message).toEqual(
-        'setDefaultBreakpoints error: Each breakpoint object should have only one key'
-      );
-    }
+    expect(() => setDefaultBreakpoints([{}])).toThrowError(/^setDefaultBreakpoints error: Each breakpoint object should have only one key$/);
+    expect(() => setDefaultBreakpoints([{ a: 1010, b: 1010 }])).toThrowError(/^setDefaultBreakpoints error: Each breakpoint object should have only one key$/);
   });
 });
