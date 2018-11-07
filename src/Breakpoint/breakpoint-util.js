bbb8b1aa7eacb52d890dcf6fbcea009597524350
Dinesh Pandiyan <flexdinesh@gmail.com>
2018-11-08 01:34:21 +0800

    Describe the default breakpoints

--- src/Breakpoint/breakpoint-util.js
+++ src/Breakpoint/breakpoint-util.js
@@ -1,11 +1,11 @@
 export class BreakpointUtil {
   constructor() {
     const defaultBreakpoints = [
-      { xsmall: 0 },
-      { small: 576 },
-      { medium: 768 },
-      { large: 992 },
-      { xlarge: 1200 }
+      { xsmall: 0 }, // all mobile devices
+      { small: 576 }, // mobile devices (not sure which one's this big)
+      { medium: 768 }, // ipad, ipad pro, ipad mini, etc
+      { large: 992 }, // smaller laptops
+      { xlarge: 1200 } // laptops and desktops
     ];
     this.allBreakpoints = defaultBreakpoints;
   }
