b7ce329c837f69cdc3832a321d53ffc3cee21efc
farhadyasir <iraz2426@gmail.com>
2019-06-24 23:21:29 +1000

    feature(typescript): Add types for typescript

---
+++ @types/index.d.ts
@@ -0,0 +1,41 @@
+import * as React from 'react';
+
+export = ReactSocks;
+export as namespace ReactSocks;
+
+declare namespace ReactSocks {
+    interface Props {
+      children?: React.ReactNode;
+      up?: boolean;
+      down?: boolean;
+      only?: boolean;
+      tagName?: string;
+      className?: string;
+    }
+
+    interface BreakpointProviderProps {
+      children: React.ReactNode;
+    }
+
+    interface BreakpointAndModifierProps {
+      breakpoint?: string;
+      modifier?: string;
+      tagName: string;
+      className: string;
+    }
+
+    export interface BreakpointContextProps {
+      currentWidth: number;
+      currentBreakpointName: string;
+    }
+  
+    export function setDefaultBreakpoints(breakpoints: any[]): void;
+
+    export class Breakpoint extends React.Component<ReactSocks.Props> {
+      static extractBreakpointAndModifierFromProps(allProps: ReactSocks.Props): ReactSocks.BreakpointAndModifierProps;
+  }
+
+    export class BreakpointProvider extends React.Component<ReactSocks.BreakpointProviderProps> {
+      static handleResize(): void;
+    }
+}
