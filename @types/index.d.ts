3e7a20685c888b24cd7c9c0f710405a306d167d1
Douglas Moore <douglas.moore@levvel.io>
2019-08-01 03:02:44 -0400

    - Updated typing for Props. - Added typing for useCurrentWidth(). - Added typing for useCurrentBreakpointName().

--- @types/index.d.ts
+++ @types/index.d.ts
@@ -4,38 +4,40 @@ export = ReactSocks;
 export as namespace ReactSocks;
 
 declare namespace ReactSocks {
-    interface Props {
-      children?: React.ReactNode;
-      up?: boolean;
-      down?: boolean;
-      only?: boolean;
-      tagName?: string;
-      className?: string;
-    }
-
-    interface BreakpointProviderProps {
-      children: React.ReactNode;
-    }
-
-    interface BreakpointAndModifierProps {
-      breakpoint?: string;
-      modifier?: string;
-      tagName: string;
-      className: string;
-    }
-
-    export interface BreakpointContextProps {
-      currentWidth: number;
-      currentBreakpointName: string;
-    }
-  
-    export function setDefaultBreakpoints(breakpoints: any[]): void;
-
-    export class Breakpoint extends React.Component<ReactSocks.Props> {
-      static extractBreakpointAndModifierFromProps(allProps: ReactSocks.Props): ReactSocks.BreakpointAndModifierProps;
+  interface Props<T = { [key: string]: boolean }> {
+    children?: React.ReactNode;
+    up?: boolean;
+    down?: boolean;
+    only?: boolean;
+    tagName?: string;
+    className?: string;
   }
 
-    export class BreakpointProvider extends React.Component<ReactSocks.BreakpointProviderProps> {
-      static handleResize(): void;
-    }
+  interface BreakpointProviderProps {
+    children: React.ReactNode;
+  }
+
+  interface BreakpointAndModifierProps {
+    breakpoint?: string;
+    modifier?: string;
+    tagName: string;
+    className: string;
+  }
+
+  export interface BreakpointContextProps {
+    currentWidth: number;
+    currentBreakpointName: string;
+  }
+
+  export function setDefaultBreakpoints(breakpoints: any[]): void;
+  export function useCurrentWidth(): number;
+  export function userCurrentBreakpointName(): string;
+
+  export class Breakpoint extends React.Component<ReactSocks.Props> {
+    static extractBreakpointAndModifierFromProps(allProps: ReactSocks.Props): ReactSocks.BreakpointAndModifierProps;
+  }
+
+  export class BreakpointProvider extends React.Component<ReactSocks.BreakpointProviderProps> {
+    static handleResize(): void;
+  }
 }
